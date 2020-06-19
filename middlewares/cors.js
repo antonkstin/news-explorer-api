dd9c70318ad441128551578ff37636ffa1739e5d
Konstantin <1konstantinmikov@gmail.com>
2020-06-19 09:28:07 +0300

    Попытка 4

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,12 @@
 function cors(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  next();
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 }
 
 module.exports = cors;
