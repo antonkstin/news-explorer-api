3f895243e28be78ad7540248c8b11e85b33cba2f
Konstantin <1konstantinmikov@gmail.com>
2020-06-22 19:09:44 +0300

    Поробовал создать запрос на удаление кук с сервера 7

--- controllers/cookie.js
+++ controllers/cookie.js
@@ -1,9 +1,8 @@
-function deleteCookie(request, response, next) {
+function deleteCookie(request, response) {
   response.cookie('jwt', 'deleted', {
     domain: '',
     maxAge: -1,
   });
-  next();
 }
 
 module.exports = deleteCookie;
