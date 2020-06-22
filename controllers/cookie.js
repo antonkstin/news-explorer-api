93cf854f7067e3ab607fdd591db69408c4e507c8
Konstantin <1konstantinmikov@gmail.com>
2020-06-22 19:07:31 +0300

    Поробовал создать запрос на удаление кук с сервера 6

--- controllers/cookie.js
+++ controllers/cookie.js
@@ -1,7 +1,7 @@
 function deleteCookie(request, response, next) {
   response.cookie('jwt', 'deleted', {
     domain: '',
-    maxAge: '1980-08-01T06:59:58.744Z',
+    maxAge: -1,
   });
   next();
 }
